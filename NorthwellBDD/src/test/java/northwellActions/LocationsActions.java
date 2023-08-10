0c7ec0e882e37ad737d4c2646c1dbda0d348ed37
nafisyasar <nafisyasar@localhost>
2023-08-10 15:56:03 -0400

    Third Commit

--- NorthwellBDD/src/test/java/northwellActions/LocationsActions.java
+++ NorthwellBDD/src/test/java/northwellActions/LocationsActions.java
@@ -1,5 +1,13 @@
 package northwellActions;
 
+import northwellElements.LocationsElements;
+
 public class LocationsActions {
+	
+	LocationsElements elements = new LocationsElements();
+	
+	public LocationsActions() {
+		
+	}
 
 }
