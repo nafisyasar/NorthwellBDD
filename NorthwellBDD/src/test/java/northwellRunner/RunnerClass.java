7081b40872dac601b8eab631f33aa877ed262dbc
nafisyasar <nafisyasar@localhost>
2023-08-14 13:47:59 -0400

    commit

--- NorthwellBDD/src/test/java/northwellRunner/RunnerClass.java
+++ NorthwellBDD/src/test/java/northwellRunner/RunnerClass.java
@@ -4,6 +4,10 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions (
+		
+		features = {"FeatureFile"},
+		glue = {"northwellStepDefs"},
+		plugin = {"pretty", "html: target/cucumberreports", "json: target/cucumber-jsonreports/cucumber.json"}
 			)
 		
 		
