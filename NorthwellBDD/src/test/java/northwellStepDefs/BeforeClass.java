b4965c7e58be7e78ddd54f31ece0e6c8639b94bf
nafisyasar <nafisyasar@localhost>
2023-08-16 14:42:08 -0400

    commit

---
+++ NorthwellBDD/src/test/java/northwellStepDefs/BeforeClass.java
@@ -0,0 +1,22 @@
+package northwellStepDefs;
+
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import northwellUtilities.WebDriverSetup;
+
+public class BeforeClass {
+	@Before
+	
+	public void beforeChromeScenario(Scenario scen) {
+		System.out.println(">>> ChromeDriver Initialization <<<");
+		try {
+			WebDriverSetup.SetUpChromeDriver();
+			System.out.println(">>> ChromeDriver Initialized");
+			System.out.println(scen.getName());
+		} catch (Exception e) {
+			System.out.println(">>> ChromeDriver Initialization has failed");
+		}
+		
+	}
+
+}
